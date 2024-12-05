5b76c0e83c048542df73dfada9c8d6ad0871c11f
malcolmdeck <malcolmdeck@gmail.com>
2024-12-05 03:43:55 -0500

    Fixing folder names

--- src/utils/Utils.kt
+++ src/utils/Utils.kt
@@ -3,7 +3,7 @@ import java.io.File
 class Utils {
     companion object {
         fun getFile(fileName: String): File {
-            return File("D:\\projects\\AdventOfCode2025\\src\\" + fileName)
+            return File("D:\\projects\\AdventOfCode2024\\src\\" + fileName)
         }
     }
 }
